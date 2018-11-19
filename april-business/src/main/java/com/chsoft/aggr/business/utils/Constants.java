dd27e7d45ea3fc5811e1885b6aecde851124bb68
zhuhuihui <jackjboss@gmail.com>
2018-11-19 21:41:51 +0800

    arthas监控

---
+++ april-business/src/main/java/com/chsoft/aggr/business/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.chsoft.aggr.business.utils;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @auhtor jacktomcat
+ * @create 2018-11-19 下午9:39
+ */
+public class Constants {
+
+    public final static Integer USER_MANAGE = 1;
+    public final static Integer USER_SUPER = 2;
+
+    public final static String USER_MANAGE_S = "1";
+    public final static String USER_SUPER_S = "2";
+
+}
